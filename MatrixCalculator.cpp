470671b260cdadebfa6c9c4461e47322f337842f
Benjamin Swenson <swen0754@umn.edu>
2020-06-13 12:28:58 -0500

    Added exception test

--- MatrixCalculator.cpp
+++ MatrixCalculator.cpp
@@ -5,8 +5,10 @@
 
 int main(){
 	std::vector<std::vector<double>> v{{1.1, 2.2, 3.3}, {4.4, 5.5, 6.6}, {7.7, 8.8, 9.9}};
+	std::vector<std::vector<double>> b{{1.0, 2.0}, {3.0, 4.0}};
 	
 	Matrix V(v);
+	Matrix B(b);
 	
 	V.print();
 	
@@ -15,6 +17,9 @@ int main(){
 	Matrix A = V + V;
 	
 	A.print();
+	
+	// Should abort after this line, as V is 3x3 and B is 2x2
+	Matrix Q = V + B;
 
 	return 0;
 }
