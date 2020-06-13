3deacbb6cb711657502d7942a9f85f6eb9cd372f
Benjamin Swenson <swen0754@umn.edu>
2020-06-13 12:21:15 -0500

    "+" Operator added

--- MatrixCalculator.cpp
+++ MatrixCalculator.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "matrix.h"
 
 int main(){
-	std::cout << "Hello World" << std::endl;
+	std::vector<std::vector<double>> v{{1.1, 2.2, 3.3}, {4.4, 5.5, 6.6}, {7.7, 8.8, 9.9}};
+	
+	Matrix V(v);
+	
+	V.print();
+	
+	std::cout << "\n";
+	
+	Matrix A = V + V;
+	
+	A.print();
 
 	return 0;
 }
--- matrix.h
+++ matrix.h
@@ -1,15 +1,17 @@
 #include <vector>
+#include <iostream>
+#include <stdexcept>
+#include <string>
 
 class Matrix{
 	private:
 		int rows;
 		int columns;
-		
 	public:
 		std::vector<std::vector<double>> matrix;
 		
 		Matrix(){
-			matrix = std::vector<std::vector<double>> ();
+			matrix = std::vector<std::vector<double>> {{}};
 			rows = 0;
 			columns = 0;
 		}
@@ -18,5 +20,27 @@ class Matrix{
 			matrix = input;
 			rows = matrix.size();
 			columns = matrix[0].size();
+		}
+		
+		void print(){
+			for(int i = 0; i < rows; i++){
+				for(int j = 0; j < columns; j++){
+					std::cout << matrix[i][j] << " ";
+				}
+				std::cout << "\n";
+			}
+		}
+		
+		Matrix operator + (const Matrix &obj){
+			if(rows != obj.rows || columns != obj.columns){
+				throw std::invalid_argument("Matricies are not of the same dimensions");
+			}
+			Matrix result(obj.matrix);
+			for(int i = 0; i < rows; i++){
+				for(int j = 0; j < columns; j++){
+					result.matrix[i][j] += matrix[i][j];
+				}
+			}
+			return result;
 		}
 };
